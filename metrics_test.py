97482c744cba33ce10a89bf9870696e0d20237f1
Shreya <shry678@gmail.com>
2023-05-01 00:10:05 -0500

    updated unit tests

--- metrics_test.py
+++ metrics_test.py
@@ -8,7 +8,6 @@ import json
 base_url = metrics.base_url
 access_token = metrics.access_token
 user = metrics.user
-# tc = unittest.TestCase()
 
 
 class MetricsTest(unittest.TestCase):
@@ -21,19 +20,25 @@ class MetricsTest(unittest.TestCase):
         assert response[0]['owner']['login'] == mock_json[0]['owner']['login']
 
     
-    # def test_get_total_repos(self):
-    #     with open('test_data.json') as user_file:
-    #         data = user_file.read()
+    def test_get_total_repos(self):
+        with open('test_data.json') as user_file:
+            data = user_file.read()
         
-    #     mock_json = json.loads(data)
+        mock_json = json.loads(data)
 
-    #     assert metrics.get_total_repos(user) == mock_json['public_repos']
+        assert metrics.get_total_repos(user) == len(mock_json)
 
 
-    
+    def test_get_freq_used_lang(self):
+        with open('test_data.json') as user_file:
+            data = user_file.read()
+        
+        mock_json = json.loads(data)
+        response = metrics.authenticate(user)
+        mock_lang = metrics.get_freq_used_lang(user, mock_json)
+        lang = metrics.get_freq_used_lang(user, response)
 
-    # def test_get_freq_used_lang(self):
-
+        assert mock_lang == lang
 
 
     def test_get_most_starred(self):
@@ -68,9 +73,7 @@ class MetricsTest(unittest.TestCase):
         response = metrics.authenticate(user)
         weekly_commit = metrics.get_commit_freq(user, response)
 
-        mock_count = next(iter(mock_commit.values()))
-        count = next(iter(weekly_commit.values()))
-        assert count == mock_count
+        assert mock_commit == weekly_commit
 
 
 if __name__ == '__main__':
